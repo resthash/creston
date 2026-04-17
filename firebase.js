f1ae5ec90b48e6591e91ca95c45a58092c4f545f
resthash <unveilingnight@gmail.com>
2026-04-17 21:13:34 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -23,15 +23,15 @@ onChildAdded
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 
-const firebaseConfig = { 
-  apiKey: "AIzaSyAxm4GyDY2RSJXUI1iFpIHUGiPfiEQX9l8",
-  authDomain: "cryptwallet360.firebaseapp.com",
-  // ADD THIS LINE BELOW
-  databaseURL: "https://cryptwallet360-default-rtdb.firebaseio.com/", 
-  projectId: "cryptwallet360",
-  storageBucket: "cryptwallet360.firebasestorage.app",
-  messagingSenderId: "464447906680",
-  appId: "1:464447906680:web:471b3482cf21557437de8d"    
+const firebaseConfig = {
+  apiKey: "AIzaSyB9FhVg5fzw18luwnrvu-X9pDdyl_SanP4",
+  authDomain: "revoutpaywallet.firebaseapp.com",
+  databaseURL: "https://revoutpaywallet-default-rtdb.firebaseio.com",
+  projectId: "revoutpaywallet",
+  storageBucket: "revoutpaywallet.firebasestorage.app",
+  messagingSenderId: "161385768191",
+  appId: "1:161385768191:web:2ee1779a4af28dd36158ca",
+  measurementId: "G-FE3ZYY0HLN"
 };
 
 const app = initializeApp(firebaseConfig);
